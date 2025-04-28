273aba870ab0c7fecc122cd51d035aa47cb5eca4
DayX2 <aliumutgungor@gmail.com>
2025-04-28 10:55:11 +0000

    Add File

--- practice_tasks/variables_data_types.py
+++ practice_tasks/variables_data_types.py
@@ -1,33 +1,34 @@
 x = 5
+
+
 y = "Hello"
+
 
 a, b, c = "Apple", "Banana", "Cherry"
 
+number = 10
+price = 19.99
+name = "Tiger"
+is_big = True
 
-number = 10         
-price = 19.99       
-name = "Elephant"   
-is_large = True 
+print(type(name))
 
-print(type(price))
-
-
-sensor_id = "SENSOR_001"    
+sensor_id = "S1"        
 sensor_type = "Temperature"  
-sensor_location = "North Wing" 
+sensor_location = "East Zone" 
 
 
-animal_name = "Lion" 
-cage_number = 7
-cage_temperature = 29.5 
+animal_name = "Elephant"   
+cage_number = 12           
+temperature = 27.5          
 
 
 print("Sensor Info:")
-print("ID:", sensor_id)
-print("Type:", sensor_type)
-print("Location:", sensor_location)
+print(sensor_id)
+print(sensor_type)
+print(sensor_location)
 
 print("\nAnimal Info:")
-print("Name:", animal_name)
-print("Cage Number:", cage_number)
-print("Temperature:", cage_temperature, "°C")
+print(animal_name)
+print(cage_number)
+print(temperature)
