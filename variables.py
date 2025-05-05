6e9e3ef03fae6e1308959c85114c53bc43ed3a7c
DayX2 <aliumutgungor@gmail.com>
2025-05-05 10:48:36 +0000

    Add File

---
+++ variables.py
@@ -0,0 +1,19 @@
+
+sensor_id = "S123"
+sensor_type = "Thermometer"
+sensor_location = "Lion Cage"
+
+
+animal_name = "Lion"
+cage_number = 5
+temperature = 36.5  
+
+print("Sensor Information:")
+print("ID:", sensor_id)
+print("Type:", sensor_type)
+print("Location:", sensor_location)
+
+print("\nAnimal Information:")
+print("Name:", animal_name)
+print("Cage No:", cage_number)
+print("Cage Temperature:", temperature, "°C")
