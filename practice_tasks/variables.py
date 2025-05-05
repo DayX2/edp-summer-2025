e0ef2e44e731c065a80484109725cf9dfc894659
DayX2 <aliumutgungor@gmail.com>
2025-05-05 10:59:50 +0000

    Add File

--- practice_tasks/variables.py
+++ practice_tasks/variables.py
@@ -13,7 +13,37 @@ print("ID:", sensor_id)
 print("Type:", sensor_type)
 print("Location:", sensor_location)
 
-print("\nAnimal Information:")
+print("Animal Information:")
 print("Name:", animal_name)
 print("Cage No:", cage_number)
 print("Cage Temperature:", temperature, "°C")
+
+#Data Types
+
+# String
+animal_name = "Tiger"
+
+# Integer
+cage_number = 7
+
+# Float
+temperature = 37.2
+
+# Boolean
+is_carnivore = True
+
+# List
+sensors = ["Temperature", "Humidity", "Motion"]
+
+# Tuple
+zoo_location = ("Section A", "North Wing")
+
+# Dictionary
+sensor_info = {
+    "id": "T100",
+    "type": "Temperature",
+    "location": "Tiger Cage"
+}
+
+# NoneType
+last_check = None
