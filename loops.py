88d823cd183d853f49c35ecc7a238154bbc99fa3
DayX2 <aliumutgungor@gmail.com>
2025-05-19 11:16:20 +0000

    Add File

---
+++ loops.py
@@ -0,0 +1,31 @@
+
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+
+for letter in "Python":
+    print(letter)
+
+for i in range(5):
+    print(i)  
+
+
+for i in range(2, 6):
+    print(i)  
+
+for i in range(0, 10, 2):
+    print(i)  
+
+
+adjectives = ["big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for adj in adjectives:
+    for fruit in fruits:
+        print(adj, fruit)
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
