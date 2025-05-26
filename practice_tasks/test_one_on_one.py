4a50f067ff0656142f29aabb71204229d3bcdabd
DayX2 <aliumutgungor@gmail.com>
2025-05-26 11:18:31 +0000

    Add Class

--- practice_tasks/test_one_on_one.py
+++ practice_tasks/test_one_on_one.py
@@ -1,3 +1,25 @@
 messages = []
 
 print(messages)
+
+messages.append("Hello")
+messages.append("How R U")
+
+
+for message in messages:
+    print(message)
+
+x = messages.pop(0)
+print(messages, x)
+
+messages.pop(0)
+print(messages)
+
+class Sensor:
+    def __init__(self, name, id, sensor_type, animal):
+        self.name = name
+        self.id = id
+        self.sensor_type = sensor_type
+        self.animal = animal
+
+s1 = Sensor("Temp", 215, "Electronic", "Tiger")
